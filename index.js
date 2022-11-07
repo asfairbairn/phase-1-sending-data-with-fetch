0846bf9abd1fa0972cf809eb71b672538bac529d
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-11-07 15:56:30 -0600

    commit tree 6a29bf339dea39d8b97297a26e8f35a3f7c1f25a

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+function submitData (userName, email) {
+    let formData = {
+        name: `${userName}`,
+        email: `${email}`,
+    }
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+    fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        return object.id;
+        //return fetch("http://localhost:3000/users/object/id")
+      })
+      .catch(function (error) {
+        alert("Request failed");
+        console.log(error.message);
+      });
+    
+}
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//    .then(function (response) {
+//     return response.json();
+//     })
+//    .then(function (object) {
+//     console.log(object);
+//     })
